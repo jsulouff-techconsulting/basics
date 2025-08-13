52e3b2991134566693544a526a780e8e942c833d
jsulouff-techconsulting <joshua.sulouff@techconsulting.tech>
2025-08-13 12:38:46 -0400

    initial

---
+++ s.playground/Contents.swift
@@ -0,0 +1,15 @@
+
+let count_limit = 45
+
+var repition_counter = 0;
+for i in 0..<45 {
+    repition_counter += 1;
+    print("There will be 45 of these, starting from 0: " + String(i))
+}
+
+print("Ran " + String(repition_counter) + " times.")
+
+let word:String = "labubu"
+let floating_point = 5.333
+let chara:Character = "b"
+let wrong = false
